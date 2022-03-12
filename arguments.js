e86b04a6eb90c7f3107b53a4edde412eca212b93
topcook <codecooker1996@gmail.com>
2022-03-12 08:48:15 -0800

    I revised arguments.js file for deploy using hardhat.

--- arguments.js
+++ arguments.js
@@ -1,6 +1,6 @@
 module.exports = [
-    "PurpleEyev4_demo",
-    "PEv4_demo",
-    "ipfs://QmUj9X5dGFtjJadREmrHJYr9XLRaoPzNca1KYqUiRrvzro/",
-    "ipfs://QmdWUSuyADXVzFvecZ4nJQHhcEMkmfDoFgNBpmNpdzB5Ph/hidden.json"
+  "THero_Hero_v1",
+  "THR_Hero_v1",
+  "ipfs://QmQia3LHb5VLsz58gvJvMwYFFN1DhZ6ssGzwPJUZrj6pZh/",
+  "ipfs://QmZQFPbCYTyCLq6n7Nk78kdhSpne6U6jV7nxXphRinTYFe/hidden_hero.json"
   ];
